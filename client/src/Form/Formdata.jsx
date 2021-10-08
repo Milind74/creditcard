449d45ccdb2ab42f79eb04b884659ed552682610
Milind <milindanand74@gmail.com>
2021-10-08 15:49:40 +0530

    initiated

---
+++ client/src/Form/Formdata.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import styles from "./Form.module.css";
+
+const Formdata = () => {
+    return (
+        <>
+        <div className={styles.container} >
+            
+        </div>
+        </>
+    )
+}
+
+export default Formdata
